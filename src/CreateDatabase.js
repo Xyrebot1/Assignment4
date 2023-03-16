d59424df204a14d95e3e0d47778ac5b59d5e1b99
Xyrebot1 <36053918+Xyrebot1@users.noreply.github.com>
2023-03-15 21:14:41 -0600

    Delete src directory

--- src/CreateDatabase.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const CreateDatabase = ({set}) => {
-    return (
-        <h3>Database and Tables successfully created!</h3>
-    );
-}
--- src/Landing.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const Landing = () => {
-return (
-<h3> Posts & Tables </h3>
-);
-}
